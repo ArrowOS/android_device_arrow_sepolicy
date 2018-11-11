7753b6f330b75980be40512423a00e72b78b1b72
Bauuuuu <abansode@outlook.com>
2018-11-11 19:19:15 +0100

    rebrand to arrow
    Change-Id: I72dda5dbaa022575f74f9f180b02d219a0340bda

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -1,13 +1,13 @@
 #
 # This policy configuration will be used by all products that
-# inherit from Lineage
+# inherit from arrow
 #
 
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
-    device/lineage/sepolicy/common/public
+    device/arrow/sepolicy/common/public
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/common/private
+    device/arrow/sepolicy/common/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/common/vendor
+    device/arrow/sepolicy/common/vendor
--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -1,11 +1,11 @@
 #
 # This policy configuration will be used by all qcom products
-# that inherit from Lineage
+# that inherit from arrow
 #
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/qcom/private
+    device/arrow/sepolicy/qcom/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom/common \
-    device/lineage/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
+    device/arrow/sepolicy/qcom/common \
+    device/arrow/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
