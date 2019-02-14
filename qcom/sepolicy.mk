cc1c975dcd1324d685946f57c65229c07d4549f0
dianlujitao <dianlujitao@lineageos.org>
2019-02-14 13:28:42 +0800

    sepolicy: Dynamically build trust policy into system/vendor
     * Introduce a new board flag TARGET_USES_PREBUILT_VENDOR_SEPOLICY and
       a sepolicy variant: dynamic
     * When TARGET_USES_PREBUILT_VENDOR_SEPOLICY=true, dynamic act as
       private policy, and vendor policy is excluded in order to avoid
       conflicts (it's not integrated to final builds anyway). When the flag
       is not set, dynamic acts as vendor policy to survive from system
       image change i.e. GSI installation.
    
    Change-Id: I8bfd078d6064616c88e2c58a9fa3aa045dddf303

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -3,9 +3,21 @@
 # that inherit from arrow
 #
 
+ifeq ($(TARGET_COPY_OUT_device), device)
+ifeq ($(BOARD_deviceIMAGE_FILE_SYSTEM_TYPE),)
+TARGET_USES_PREBUILT_device_SEPOLICY ?= true
+endif
+endif
+
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/arrow/sepolicy/qcom/private
 
+ifeq ($(TARGET_USES_PREBUILT_device_SEPOLICY), true)
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/arrow/sepolicy/qcom/dynamic
+else
 BOARD_SEPOLICY_DIRS += \
-    device/arrow/sepolicy/qcom/common \
-    device/arrow/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
+    device/arrow/sepolicy/qcom/dynamic \
+    device/arrow/sepolicy/qcom/common
+endif
+
